3a4c216152510299adea81c9e3df179c93dfb942
Patrick Kelly <patomation@gmail.com>
2021-06-27 03:47:35 -0400

    feat: some custom aliases and readme

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+# Custom ZSH
+
+## Usage
+Add the following to your `.zshrc file`, (should be the path to this directory)
+```s
+ZSH_CUSTOM=~/source/zsh_custom
+```
---
+++ aliases.zsh
@@ -0,0 +1,14 @@
+# List all aliases
+alias help="alias"
+
+# Work Github Stuff
+alias sdp="git switch develop && git pull"
+alias rd="git rebase develop"
+alias pr="gh pr create -B develop"
+alias g="npm run generate"
+
+# Git
+alias switch="git switch $1"
+
+# Reload ZSH
+alias r="source ~/.zshrc"
